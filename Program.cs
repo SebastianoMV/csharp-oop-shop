2e2f3dc8432336b59cc37f532c6e0ae50b4f3a3b
SebastianoMV <sebastianovisco@gmail.com>
2022-09-19 14:50:17 +0200

    Creazione classe prodotti con set e get dei valori

--- Program.cs
+++ Program.cs
@@ -1,2 +1,96 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("OOP SHOP");
+
+
+Prodotto prodotto = new Prodotto();
+
+prodotto.getCodice();
+
+prodotto.setNome();
+
+prodotto.getNome();
+
+prodotto.getPrezzo();
+prodotto.getPrezzoConIva();
+
+prodotto.getNomeEsteso();
+
+class Prodotto
+{
+    int codice = 123;
+    string nome = "Scarpe";
+    string descrizione = "scarpe blu taglia 44";
+    decimal prezzo = 50.99m;
+    int iva = 22;
+
+
+    //GET
+    public void getCodice()
+    {
+        Console.WriteLine(codice);
+    }
+
+    public void getNome()
+    {
+        Console.WriteLine(nome);
+    }
+
+    public void getDescrizione()
+    {
+        Console.WriteLine(descrizione);
+    }
+
+    public void getPrezzo()
+    {
+        Console.WriteLine(prezzo);
+    }
+
+
+    public void getIva()
+    {
+        Console.WriteLine(iva);
+    }
+
+    //SET
+
+    public string setNome()
+    {
+        nome = Console.ReadLine();
+        return nome;
+    }
+
+    public string setDescrizione()
+    {
+        descrizione = Console.ReadLine();
+        return descrizione;
+    }
+
+    public decimal setPrezzo()
+    {
+        prezzo = decimal.Parse(Console.ReadLine());
+        return prezzo;
+    }
+
+    public int setIva()
+    {
+        iva = Int32.Parse(Console.ReadLine());
+        return iva;
+    }
+
+
+    // GET PREZZO CON IVA
+    public void getPrezzoConIva()
+    {
+        decimal prezzoiva = Math.Round(prezzo + ((prezzo * 22) / 100), 2);
+        Console.WriteLine("Prezzo con Iva: " + prezzoiva);
+    }
+
+    // GET NOME ESTESO
+    public void getNomeEsteso()
+    {
+        string nomeEsteso = codice + nome;
+        Console.WriteLine("Nome esteso: " + nomeEsteso);
+    }
+};
+
+
