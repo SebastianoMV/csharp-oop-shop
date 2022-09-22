69c39377868d430ec0239bf88207edc4686a8518
SebastianoMV <sebastianovisco@gmail.com>
2022-09-22 14:58:19 +0200

    Trasformazione di Prodotto in Abstract e creazione nuove classi

--- Prodotto.cs
+++ Prodotto.cs
@@ -1,99 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Prodotto
+abstract class Prodotto
 {
-    int codice = 123;
-    string nome;
-    string descrizione;
-    double prezzo;
-    int iva;
+    public int Codice { get; set; }
+    public string Nome { get; set; }
+    public string Descrizione { get; set; }
+    public double Prezzo { get; set; }
+    public int Iva { get; set; }
 
 
     //COSTRUTTORE PRODOTTO
     public Prodotto(string nome, string descrizione, double prezzo, int iva)
     {
         Random random = new Random( );
-        this.codice = random.Next(1, 123445);
-        this.nome = nome;
-        this.descrizione = descrizione;
-        this.prezzo = prezzo;
-        this.iva = iva;
+        Codice = random.Next(1, 123445);
+        Nome = nome;
+        Descrizione = descrizione;
+        Prezzo = prezzo;
+        Iva = iva;
     }
 
     //GET
 
     public void getProdotto()
     {
-        Console.WriteLine("Codice prodotto: " + codice);
-        Console.WriteLine("Nome prodotto: " + nome);
-        Console.WriteLine("Descrizione prodotto: " + descrizione);
-        Console.WriteLine("Prezzo prodotto: " + prezzo);
-        Console.WriteLine("Iva prodotto: " + iva);
+        Console.WriteLine("Codice prodotto: " + Codice);
+        Console.WriteLine("Nome prodotto: " + Nome);
+        Console.WriteLine("Descrizione prodotto: " + Descrizione);
+        Console.WriteLine("Prezzo prodotto: " + Prezzo);
+        Console.WriteLine("Iva prodotto: " + Iva);
     }
     public void getCodice()
     {
-        Console.WriteLine(codice);
-    }
-
-    public void getNome()
-    {
-        Console.WriteLine(nome);
-    }
-
-    public void getDescrizione()
-    {
-        Console.WriteLine(descrizione);
-    }
-
-    public void getPrezzo()
-    {
-        Console.WriteLine(prezzo);
-    }
-
-
-    public void getIva()
-    {
-        Console.WriteLine(iva);
-    }
-
-    //SET
-
-    public string setNome(string nomeNuovo)
-    {
-        nome = nomeNuovo;
-        return nome;
-    }
-
-    public string setDescrizione(string descrizioneNuova)
-    {
-        descrizione = descrizioneNuova;
-        return descrizione;
-    }
-
-    public double setPrezzo(double prezzoNuovo)
-    {
-        prezzo = prezzoNuovo;
-        return prezzo;
-    }
-
-    public int setIva(int ivaNuova)
-    {
-        iva = ivaNuova;
-        return iva;
+        Console.WriteLine(Codice);
     }
 
 
     // GET PREZZO CON IVA
     public void getPrezzoConIva()
     {
-        double prezzoiva = Math.Round(prezzo + ((prezzo * iva) / 100), 2);
+        double prezzoiva = Math.Round(Prezzo + ((Prezzo * Iva) / 100), 2);
         Console.WriteLine("Prezzo con Iva: " + prezzoiva);
     }
 
     // GET NOME ESTESO
-    public void getNomeEsteso()
+    public virtual void getNomeEsteso()
     {
-        string nomeEsteso = codice + nome;
+        string nomeEsteso = Codice + Nome;
         Console.WriteLine("Nome esteso: " + nomeEsteso);
     }
 
@@ -101,13 +54,70 @@ class Prodotto
     public void getCodicePad()
     {
        
-        string codicePad = codice.ToString();
+        string codicePad = Codice.ToString();
         while(codicePad.Length < 8)
         {
             codicePad = "0" + codicePad;
         }
         Console.WriteLine(codicePad);
     }
+
+
+    //METODI ASTRATTI
 };
 
 
+class Acqua : Prodotto
+{
+    public double Litri { get; set; }
+    public double Ph { get; set; }
+
+    
+
+    public Acqua(string nome, string descrizione, double prezzo, int iva, double litri, double ph) : base(nome, descrizione, prezzo, iva)
+    {
+        this.Litri = Litri;
+        this.Ph = ph;
+    }
+
+    public override void getNomeEsteso()
+    {
+        base.getNomeEsteso();
+        Console.WriteLine("Livello ph: " + Ph);
+        Console.WriteLine("Litri: " + Litri);
+    }
+}
+
+class ConfezioneAcqua : Acqua
+{
+    public int NumeroBottiglie{ get; set; }
+    public ConfezioneAcqua(string nome, string descrizione, double prezzo, int iva, double litri, double ph, int numeroBottiglie) : base(nome, descrizione, prezzo, iva, litri, ph)
+    {
+        NumeroBottiglie = numeroBottiglie;
+    }
+
+    public override void getNomeEsteso()
+    {
+        base.getNomeEsteso();
+        Console.WriteLine("Confezione da " + NumeroBottiglie + " bottiglie");
+    }
+}
+
+class Frutta : Prodotto
+{
+    public string Varietà { get; set; }
+
+    public Frutta(string nome, string descrizione, double prezzo, int iva, string varietà) : base(nome, descrizione, prezzo, iva)
+    {
+        Varietà = varietà;
+    }
+}
+
+class SacchettoFrutta : Frutta
+{
+    public int Quantità { get; set; }
+    public SacchettoFrutta(string nome, string descrizione, double prezzo, int iva, string varietà, int quantità) : base(nome, descrizione, prezzo, iva, varietà)
+    {
+        Quantità = quantità;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,66 +2,66 @@
 Console.WriteLine("OOP SHOP");
 
 
-Prodotto prodotto = new Prodotto("pippo", "ciao come stai", 20.99 , 22);
-prodotto.getCodicePad();
+//Prodotto prodotto = new Prodotto("pippo", "ciao come stai", 20.99 , 22);
+//prodotto.getCodicePad();
 
-prodotto.getProdotto();
+//prodotto.getProdotto();
 
-prodotto.getPrezzoConIva();
+//prodotto.getPrezzoConIva();
 
-prodotto.getNomeEsteso();
+//prodotto.getNomeEsteso();
 
-bool flagChange = false;
-string scelta;
+//bool flagChange = false;
+//string scelta;
 
-Console.WriteLine("vuoi cambiare qualcosa? [si,no]");
-scelta = Console.ReadLine();
-if(scelta == "si")
-{
-    flagChange = true;
-};
+//Console.WriteLine("vuoi cambiare qualcosa? [si,no]");
+//scelta = Console.ReadLine();
+//if(scelta == "si")
+//{
+//    flagChange = true;
+//};
 
-while (flagChange)
-{
-    Console.WriteLine("Cosa vuoi cambiare? [nome,descrizione,prezzo,iva]");
-    scelta= Console.ReadLine();
-    if (scelta == "nome")
-    {
-        string s = Console.ReadLine();
-        prodotto.setNome(s);
+//while (flagChange)
+//{
+//    Console.WriteLine("Cosa vuoi cambiare? [nome,descrizione,prezzo,iva]");
+//    scelta= Console.ReadLine();
+//    if (scelta == "nome")
+//    {
+//        string s = Console.ReadLine();
+//        prodotto.setNome(s);
 
-    }else if (scelta == "descrizione")
-    {
-        string s = Console.ReadLine();
-        prodotto.setDescrizione(s);
+//    }else if (scelta == "descrizione")
+//    {
+//        string s = Console.ReadLine();
+//        prodotto.setDescrizione(s);
 
-    }
-    else if (scelta == "prezzo")
-    {
-        double prezzoNew = double.Parse(Console.ReadLine());
-        prodotto.setPrezzo(prezzoNew);
-    }
-    else if (scelta == "iva")
-    {
-        int ivaNew = Int32.Parse(Console.ReadLine());
-        prodotto.setIva(ivaNew);
-    }
-    else
-    {
-        Console.WriteLine("Scelta non valida.");
-    }
+//    }
+//    else if (scelta == "prezzo")
+//    {
+//        double prezzoNew = double.Parse(Console.ReadLine());
+//        prodotto.setPrezzo(prezzoNew);
+//    }
+//    else if (scelta == "iva")
+//    {
+//        int ivaNew = Int32.Parse(Console.ReadLine());
+//        prodotto.setIva(ivaNew);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Scelta non valida.");
+//    }
 
-    Console.WriteLine("vuoi cambiare altro? [si,no]");
-    scelta = Console.ReadLine();
+//    Console.WriteLine("vuoi cambiare altro? [si,no]");
+//    scelta = Console.ReadLine();
 
-    if (scelta == "no")
-    {
-        flagChange = false;
-        prodotto.getProdotto();
-    };
+//    if (scelta == "no")
+//    {
+//        flagChange = false;
+//        prodotto.getProdotto();
+//    };
 
     
-};
+//};
 
 
 
