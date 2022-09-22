0feb16d98c41e5ac219ebfff22170a4e9a4fab7e
SebastianoMV <sebastianovisco@gmail.com>
2022-09-22 17:17:54 +0200

    Prime prove carrello

--- Prodotto.cs
+++ Prodotto.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.CompilerServices;
+
 abstract class Prodotto
 {
+    // galloni è un parametro statico che posso usare i tutte le sottoclassi che prevedono dei prodotti con litri
+    public static double galloni = 3.785;
     public int Codice { get; set; }
     public string Nome { get; set; }
     public string Descrizione { get; set; }
@@ -22,7 +26,7 @@ abstract class Prodotto
 
     //GET
 
-    public void getProdotto()
+    public virtual void getProdotto()
     {
         Console.WriteLine("Codice prodotto: " + Codice);
         Console.WriteLine("Nome prodotto: " + Nome);
@@ -35,6 +39,8 @@ abstract class Prodotto
         Console.WriteLine(Codice);
     }
 
+    //public abstract double prezzoConf();
+
 
     // GET PREZZO CON IVA
     public void getPrezzoConIva()
@@ -44,7 +50,7 @@ abstract class Prodotto
     }
 
     // GET NOME ESTESO
-    public virtual void getNomeEsteso()
+    public  void getNomeEsteso()
     {
         string nomeEsteso = Codice + Nome;
         Console.WriteLine("Nome esteso: " + nomeEsteso);
@@ -63,44 +69,57 @@ abstract class Prodotto
     }
 
 
-    //METODI ASTRATTI
+
 };
 
 
 class Acqua : Prodotto
 {
     public double Litri { get; set; }
-    public double Ph { get; set; }
-
-    
+    public double Ph { get; }
 
     public Acqua(string nome, string descrizione, double prezzo, int iva, double litri, double ph) : base(nome, descrizione, prezzo, iva)
     {
-        this.Litri = Litri;
+        this.Litri = litri;
         this.Ph = ph;
     }
 
-    public override void getNomeEsteso()
+    public override void getProdotto()
     {
-        base.getNomeEsteso();
+        base.getProdotto();
         Console.WriteLine("Livello ph: " + Ph);
         Console.WriteLine("Litri: " + Litri);
     }
+
+    public static double getGalloni(double litri)
+    {
+        return galloni * litri;
+    }
+
+
 }
 
 class ConfezioneAcqua : Acqua
 {
     public int NumeroBottiglie{ get; set; }
+    public double PrezzoConfezione { get;}
     public ConfezioneAcqua(string nome, string descrizione, double prezzo, int iva, double litri, double ph, int numeroBottiglie) : base(nome, descrizione, prezzo, iva, litri, ph)
     {
         NumeroBottiglie = numeroBottiglie;
+        PrezzoConfezione = prezzoConf();
     }
 
-    public override void getNomeEsteso()
+    public override void getProdotto()
     {
-        base.getNomeEsteso();
+        base.getProdotto();
         Console.WriteLine("Confezione da " + NumeroBottiglie + " bottiglie");
     }
+
+    public double prezzoConf()
+    {
+        return  Prezzo * NumeroBottiglie;
+    }
+    
 }
 
 class Frutta : Prodotto
@@ -111,13 +130,33 @@ class Frutta : Prodotto
     {
         Varietà = varietà;
     }
+
+    public override void getProdotto()
+    {
+        base.getProdotto();
+        Console.WriteLine("Confezione da " + Varietà + " bottiglie");
+    }
 }
 
 class SacchettoFrutta : Frutta
 {
     public int Quantità { get; set; }
+    public double PrezzoConfezione { get;}
     public SacchettoFrutta(string nome, string descrizione, double prezzo, int iva, string varietà, int quantità) : base(nome, descrizione, prezzo, iva, varietà)
     {
         Quantità = quantità;
+        PrezzoConfezione = prezzoConf();
+    }
+
+    public override void getProdotto()
+    {
+        base.getProdotto();
+        Console.WriteLine("Confezione da " + Quantità + " bottiglie");
+    }
+
+    public double prezzoConf()
+    {
+        return Prezzo * Quantità;
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("OOP SHOP");
 
+Acqua acqua = new Acqua("San Benedetto", "Acqua naturale oligominerale", 1.50, 10, 1.5, 5.50);
+ConfezioneAcqua confAcqua = new ConfezioneAcqua("San Benedetto Confezione", "Acqua naturale oligominerale", 1.50, 10, 1.5, 5.50, 6);
+Frutta pesca = new Frutta("pesca", "frutto", 1.5, 4, "bianca");
+SacchettoFrutta confPesche = new SacchettoFrutta("Confezione di pesche", "frutto", 1.5, 4, "bianca", 6); 
+
+
+
+List<Prodotto> carrello = new List<Prodotto>();
+
+Console.WriteLine("Benvenuto! Cosa vuoi acquistare?");
+bool flag = true;
+
+while (flag)
+{
+    Console.WriteLine("1. Bottiglia d'acqua");
+    Console.WriteLine("2. Confezione d'acqua");
+    Console.WriteLine("3. Una pesca");
+    Console.WriteLine("4. Un sacchetto di pesche");
+    int scelta = Int32.Parse(Console.ReadLine());
+
+    switch (scelta)
+    {
+        case 1:
+            carrello.Add(acqua);
+            Console.WriteLine("Acqua aggiunta al carrello");
+            break;
+        case 2:
+            carrello.Add(confAcqua);
+            Console.WriteLine("Confezione d'acqua aggiunta al carrello");
+            break;
+        case 3:
+            carrello.Add(pesca);
+            Console.WriteLine("Pesca aggiuta al carrello");
+            break;
+        case 4:
+            carrello.Add(confPesche);
+            Console.WriteLine("Sacchetto di pesche aggiunto al carrello");
+            break;
+        default:
+            Console.WriteLine("Prodotto inesistente");
+            break;
+    }
+    Console.WriteLine("Vuoi aggiungere altri prodotti? [si/no]");
+    string scelta2 = Console.ReadLine();
+    if (scelta2 == "no") flag = false;
+}
+
+
+foreach (Prodotto car in carrello)
+{
+    car.getProdotto();
+}
+
+
+
+
+
+
+
+
+
 
 //Prodotto prodotto = new Prodotto("pippo", "ciao come stai", 20.99 , 22);
 //prodotto.getCodicePad();
@@ -60,7 +121,7 @@ Console.WriteLine("OOP SHOP");
 //        prodotto.getProdotto();
 //    };
 
-    
+
 //};
 
 
